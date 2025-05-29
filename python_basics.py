5a637af9894233b71229e3c047b7b296d092f30c
liudas-bajer <l.bajer@gebhardt-group.com>
2025-05-29 19:30:48 +0300

    print out some more private information

--- python_basics.py
+++ python_basics.py
@@ -1,4 +1,16 @@
+def bool_to_yes_no(value: bool) -> str:
+    return "Yes" if value else "No"
 
+salutation = "Hey Heyyyyyyyyyyiiiii!!!"
+age = 15
+height = 182
+is_old_enough_to_vote = age >= 18
+is_married = True
 
 # sekancioje ejluteje programa turetu isspausdinti pasisveikinima.
-print("Hey Heyyyyyyyyyyiiiii!!!")
+print(f"{salutation} My age is {age} years and height {height} centimeters")
+print(f"am i old enough to vote? {bool_to_yes_no(is_old_enough_to_vote)}")
+print(f"Bin ich verheiratet? {bool_to_yes_no(is_married)}")
+
+
+
